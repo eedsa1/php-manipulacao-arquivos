988bd9199ed8220f5d923cb81d2c88d8a5d27abb
= <=>
2020-01-30 22:30:55 -0300

    adição de arquivos com uso de funções de upload, download etc

---
+++ exemplo09.php
@@ -0,0 +1,30 @@
+<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="fileUpload">
+	<button type="submit">Send</button>
+</form>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+	$file = $_FILES["fileUpload"];
+
+	if($file["error"]){
+		throw new Exception("error: ".$file["error"]);
+	}
+
+	$dirUploads = "uploads";
+
+	if(!is_dir($dirUploads)){
+		mkdir($dirUploads);
+	}
+
+	if(move_uploaded_file($file["tmp_name"], $dirUploads.DIRECTORY_SEPARATOR.$file["name"])){
+		echo "upload realizado com sucesso!";
+	}
+	else{
+		throw new Exception("não foi possível realizar o upload", 1);
+	}
+}
+
+
+?>
